95052abcd6b6450cf0eed83e37481a2685347dbe
jeremite <543084991@qq.com>
2021-01-25 23:01:25 -0800

    project initialization

---
+++ src/main/java/com/rsvps/responsepred/ResponsePredApplication.java
@@ -0,0 +1,13 @@
+package com.rsvps.responsepred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponsePredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponsePredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsvps/responsepred/ResponsePredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsvps.responsepred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponsePredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
